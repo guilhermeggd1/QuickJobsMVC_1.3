a2d4c0cb1cc29ca746ac2c40cd9f882ebe8a5a53
Beatriz Ribeiro <maria_beatriz_nunes_ribeiro@carrefour.com>
2020-04-23 16:57:29 -0300

    Projeto criado

---
+++ QuickJobsMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QuickJobsMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
